784c71bdcb2860e8523c3c0b0373ba4d00705362
Mikhail Kiselev <mihail.kiselev@gmail.com>
2025-02-13 20:06:31 +0200

    List of recipes is working

---
+++ src/testRecipeBook.java
@@ -0,0 +1,10 @@
+// Recipe book serialization test
+
+public class testRecipeBook {
+	private static RecipeBook recipeBook = new RecipeBook();
+	public static void main(String[] args) {
+		recipeBook.recipeBookSave();
+
+
+	}
+}

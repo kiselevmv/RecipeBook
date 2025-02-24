49b263b177f6ddd4c803c087230ab08574481db1
Mikhail Kiselev <mihail.kiselev@gmail.com>
2025-02-24 21:03:53 +0200

    Remove unnessesary files

--- JavaFX/build.sh
+++
@@ -1,1 +0,0 @@
-javac --module-path /Library/Java/JavaVirtualMachines/javafx-sdk-23.0.2/lib/ --add-modules javafx.controls,javafx.fxml RecipeBookFX.java
--- JavaFX/run.sh
+++
@@ -1,1 +0,0 @@
-java --module-path /Library/Java/JavaVirtualMachines/javafx-sdk-23.0.2/lib/ --add-modules javafx.controls,javafx.fxml RecipeBookFX
